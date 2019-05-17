406a59081868333055b7fbf46914a723156e75b8
Robert Ridal <ridal.robert@gmail.com>
2019-05-17 21:14:32 +0300

    Added blog post to page. This is hardcoded

--- test_project/testblog/views.py
+++ test_project/testblog/views.py
@@ -4,13 +4,30 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Robert',
+        'title': 'Robertin Blogi',
+        'content': 'Test Blog Post',
+        'date_posted': 'Syyskuuta 22, 1994'
+    },
+    {
+        'author': 'Sinkki',
+        'title': 'Sinkin Blogi',
+        'content': 'Test Blog Post Toinen',
+        'date_posted': 'Uusikuusi 22, 1991'
+    }
+]
 
 '''def home(request):
     return HttpResponse('<H1>First View - Home</h1>')'''
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 '''When I tried to do \'blog/home.html\' it said template does not exist so I had to just type \'home.html\'. But I 
